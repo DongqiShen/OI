0994abccd3116121c98e5048288da6c8d20bdca1
Dongqi Shen <dq_shen@outlook.com>
2021-09-25 18:24:37 +0800

    前缀和+树+回溯

--- debug.cpp
+++ debug.cpp
@@ -3,63 +3,69 @@
  * Date: 2021.09.12
  * Author: 觉不醒的王子殿下
  */
+#include <limits.h>
+
+#include <algorithm>
+#include <fstream>
 #include <iostream>
-#include <vector>
 #include <queue>
 #include <stack>
-#include <limits.h>
-#include <unordered_set>
 #include <unordered_map>
-#include <fstream>
-#include <algorithm>
+#include <unordered_set>
+#include <vector>
 
 using namespace std;
-class Solution
-{
-private:
-    // 邻接表：graph[x][y] = c表示x/y=c，需要注意的是分子分母交换位子也是一条边
-    unordered_map<string, unordered_map<string, double>> graph;
-    // 表示节点的名字（唯一）
-    unordered_set<string> nodes;
-public:
-    vector<double> calcEquation(vector<vector<string>> &equations, vector<double> &values, vector<vector<string>> &queries)
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode()
+        : val(0)
+        , left(nullptr)
+        , right(nullptr)
     {
-        int n = equations.size();
-        // 建立邻接表
-        for(int i = 0; i < n; ++i){
-            string a = equations[i][0];
-            string b = equations[i][1];
-            double value = values[i];
-            graph[a][b] = value;
-            graph[b][a] = 1.0/value;
-            nodes.insert(a);
-            nodes.insert(b);
-        }
-        // floyd算法，找出每个节点之间的（最短）距离，在这道题目里面，两点之间
-        // 的权重唯一，要么不存在，存在就是唯一
-        // k表示中间节点，从i->k->j的距离
-        for(auto &k : nodes){
-            for(auto &i : nodes){
-                for(auto &j : nodes){
-                    // 如果i->k和k->j之间存在边
-                    if(graph[i].count(k) && graph[k].count(j)){
-                        graph[i][j] = graph[i][k] * graph[k][j];
-                    }
-                }
-            }
-        }
-        vector<double> ans;
-        for(auto& q : queries){
-            if(!graph[q[0]][q[1]]){
-                ans.push_back(-1.0);
-            }else{
-                ans.push_back(graph[q[0]][q[1]]);
-            }
-        }
-        return ans;
+    }
+    TreeNode(int x)
+        : val(x)
+        , left(nullptr)
+        , right(nullptr)
+    {
+    }
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x)
+        , left(left)
+        , right(right)
+    {
     }
 };
 
+class Solution {
+public:
+    int recursionPathSum(TreeNode* root, unordered_map<int, int>& prefixCount, 
+                        int targetSum, int curSum)
+    {
+        if(root == nullptr){
+            return 0;
+        }
+        int res = 0;
+        curSum += root->val;
+        if(prefixCount.count(curSum - targetSum)){
+            res += prefixCount[curSum - targetSum];
+        }
+        prefixCount[curSum] += 1;
+        res += recursionPathSum(root->left, prefixCount, targetSum, curSum);
+        res += recursionPathSum(root->right, prefixCount, targetSum, curSum);
+        prefixCount[curSum] -= 1;
+        return res;
+    }
+    int pathSum(TreeNode* root, int targetSum)
+    {
+        unordered_map<int, int> prefixCount;
+        prefixCount[0] = 1;
+        return recursionPathSum(root, prefixCount, targetSum, 0);
+    }
+};
 
 int main()
 {
