126ad1cf44abe341add5d3230ab7e99e8b93a300
Dongqi Shen <dq_shen@outlook.com>
2021-09-27 00:08:05 +0800

    494 01背包

--- debug.cpp
+++ debug.cpp
@@ -4,7 +4,6 @@
  * Author: 觉不醒的王子殿下
  */
 #include <limits.h>
-
 #include <algorithm>
 #include <fstream>
 #include <iostream>
@@ -13,62 +12,40 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode()
-        : val(0)
-        , left(nullptr)
-        , right(nullptr)
-    {
-    }
-    TreeNode(int x)
-        : val(x)
-        , left(nullptr)
-        , right(nullptr)
-    {
-    }
-    TreeNode(int x, TreeNode* left, TreeNode* right)
-        : val(x)
-        , left(left)
-        , right(right)
-    {
-    }
-};
-
 class Solution {
 public:
-    int recursionPathSum(TreeNode* root, unordered_map<int, int>& prefixCount, 
-                        int targetSum, int curSum)
+    int findTargetSumWays(vector<int>& nums, int target)
     {
-        if(root == nullptr){
+        int target_sum = accumulate(nums.begin(), nums.end(), target);
+        if(target_sum < 0 || target_sum & 1){
             return 0;
         }
-        int res = 0;
-        curSum += root->val;
-        if(prefixCount.count(curSum - targetSum)){
-            res += prefixCount[curSum - targetSum];
+        target_sum /= 2;
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(target_sum + 1, 0));
+        dp[0][0] = 1;
+        for(int i = 1; i <= n; ++i){
+            int num = nums[i-1];
+            for(int j = 0; j <= target_sum; ++j){
+                dp[i][j] = dp[i-1][j];
+                if(j >= num){
+                    dp[i][j] += dp[i-1][j-num];
+                }
+            }
         }
-        prefixCount[curSum] += 1;
-        res += recursionPathSum(root->left, prefixCount, targetSum, curSum);
-        res += recursionPathSum(root->right, prefixCount, targetSum, curSum);
-        prefixCount[curSum] -= 1;
-        return res;
-    }
-    int pathSum(TreeNode* root, int targetSum)
-    {
-        unordered_map<int, int> prefixCount;
-        prefixCount[0] = 1;
-        return recursionPathSum(root, prefixCount, targetSum, 0);
+        return dp[n][target_sum];
     }
 };
 
 int main()
 {
     Solution solution;
+    vector<int> nums = {100};
+    int target = 3;
+    int ret = solution.findTargetSumWays(nums, target);
     return 0;
 }
