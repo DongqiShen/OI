6da7b183d0b7a238db3936568bb502a1fb1845a7
Dongqi Shen <dq_shen@outlook.com>
2021-10-05 09:45:22 +0800

    124\209

--- debug.cpp
+++ debug.cpp
@@ -9,75 +9,97 @@
 #include <iostream>
 #include <queue>
 #include <stack>
+#include <deque>
+#include <map>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
 #include <numeric>
+#include <cstring>
 
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+ifstream fin("input.txt");
+ofstream fout("output.txt");
 
-class Solution {
-public:
-    ListNode* merge(ListNode* head1, ListNode* head2){
-        ListNode* dummyNode = new ListNode();
-        ListNode* ptr = dummyNode;
-        while(head1 && head2){
-            if(head1->val <= head2->val){
-                ptr->next = head1;
-                head1 = head1->next;
-            }else{
-                ptr->next = head2;
-                head2 = head2->next;
-            }
-            ptr = ptr->next;
+const int maxn = 1000;
+deque<int> readyQ;
+queue<int> blockQ;
+int n, quantum, c[5], var[26] = {0}, ip[maxn];
+bool locked;
+char prog[maxn][10];
+
+void run(int pid){
+    int q = quantum;
+    while(q > 0) {
+        char* p = prog[ip[pid]];
+        switch (p[2]){
+            case '=':
+                var[p[0] - 'a'] = isdigit(p[5]) ? (p[4] - '0') * 10 + p[5] - '0' : p[4] - '0';
+                q -= c[0];
+                break;
+            case 'i':
+                fout<<pid + 1<<": "<<var[p[6]-'a']<<"\n";
+                q -= c[1];
+                break;
+            case 'c':
+                if(locked){
+                    blockQ.push(pid);
+                    return;
+                }
+                locked = true;
+                q -= c[2];
+                break;
+            case 'l':
+                locked = false;
+                if(!blockQ.empty()){
+                    int pid2 = blockQ.front();
+                    blockQ.pop();
+                    readyQ.push_front(pid2);
+                }
+                q -= c[3];
+                break;
+            case 'd':
+                return;
         }
-        if(head1 != nullptr){
-            ptr->next = head1;
-        }else if(head2 != nullptr){
-            ptr->next = head2;
-        }
-        return dummyNode->next;
+        ip[pid]++;
     }
-    ListNode* sortList(ListNode* head)
-    {
-        if(head == nullptr){
-            return nullptr;
-        }
-        if(head->next == nullptr){
-            return head;
-        }
-        ListNode* dummyNode = new ListNode();
-        dummyNode->next = head;
-        ListNode* slow = dummyNode;
-        ListNode* fast = dummyNode;
-        while(fast && fast->next){
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        ListNode* last = slow->next;
-        slow->next = nullptr;
-        ListNode* left = sortList(head);
-        ListNode* right = sortList(last);
-        return merge(left, right);
-    }
-};
+    readyQ.push_back(pid);
+}
+
 
 int main()
 {
-    ListNode* a = new ListNode(4);
-    ListNode* b = new ListNode(2);
-    ListNode* c = new ListNode(1);
-    ListNode* d = new ListNode(3);
-    a->next = b; b->next = c; c->next = d; d->next = nullptr;
-    Solution solution;
-    ListNode* ret = solution.sortList(a);
+    int T;
+    fin>>T;
+    while(T--){
+        fin>>n>>c[0]>>c[1]>>c[2]>>c[3]>>c[4]>>quantum;
+        memset(var, 0, sizeof(var));
+        int line = 0;
+        for(int i = 0; i < n; ++i){
+            fin.getline(prog[line++], maxn);
+            while(prog[line-1][0] == '\0'){
+                fin.getline(prog[line-1], maxn);
+            }
+            ip[i] = line - 1;
+            while(prog[line-1][2] != 'd'){
+                fin.getline(prog[line++], maxn);
+                if(prog[line-1][0] == '\0'){
+                    line--;
+                }
+            }
+            readyQ.push_back(i);
+        }
+
+        locked = false;
+        while(!readyQ.empty()){
+            int pid = readyQ.front();
+            readyQ.pop_front();
+            run(pid);
+        }
+        if(T){
+            fout<<"\n";
+        }
+    }
     return 0;
 }
