aebcbaa74191e06dad95f9e7b1206a13cb53a8c1
Dongqi Shen <dq_shen@outlook.com>
2022-01-16 00:06:34 +0800

    多路归并专题

--- debug.cpp
+++ debug.cpp
@@ -20,59 +20,35 @@
 #include <vector>
 
 using namespace std;
-
 class Solution {
 public:
-    vector<int> diffWaysToCompute(string expression)
+    int smallestDistancePair(vector<int>& nums, int k)
     {
-        int n = expression.size();
-        vector<char> ops;
-        vector<int> nums;
-        string str = "";
-        for (int i = 0; i < n; ++i) {
-            char ch = expression[i];
-            if (isdigit(ch)) {
-                str += ch;
-            } else {
-                nums.emplace_back(stoi(str));
-                ops.emplace_back(ch);
-                str.clear();
-            }
-        }
-        nums.emplace_back(stoi(str));
-        n = nums.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n));
-        for (int i = 0; i < n; ++i) {
-            dp[i][i].emplace_back(nums[i]);
-        }
-        for (int l = 1; l <= n; ++l) { // 区间长度
-            for (int i = 0; i + l - 1 < n; ++i) { // i：区间起点
-                int j = i + l - 1; // j：区间终点
-                for (int k = i; k < j; ++k) { // k：区间中点
-                    for (auto &left: dp[i][k]) { // left: 左区间可能的值
-                        for (auto &right: dp[k+1][j]) { // right: 右区间可能的值
-                            int result = 0;
-                            if (ops[k] == '+') {
-                                result = left + right;
-                            } else if (ops[k] == '-') {
-                                result = left - right;
-                            } else if (ops[k] == '*') {
-                                result = left * right;
-                            }
-                            dp[i][j].emplace_back(result);
-                        }
-                    }
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int left = 0, right = nums[n-1] - nums[0];
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            int count = 0;
+            int l = 0;
+            for (int r = 0; r < n; ++r) {
+                while (nums[r] - nums[l] > mid) {
+                    l++;
                 }
+                count += r - l;
+            }
+            if (count >= k) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        return dp[0][n-1];
+        return left;
     }
 };
 
 int main()
-{
+{   
     Solution solution;
-    string expression = "2-1-1";
-    vector<int> ans = solution.diffWaysToCompute(expression);
     return 0;
 }
